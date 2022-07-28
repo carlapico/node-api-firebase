5d84020aa7db5236f5fa9ea30f4f41a08f92ad52
Carla Pico <carlapico@Carlas-MacBook-Air.local>
2022-07-28 15:48:25 -0400

    deployed cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,27 @@
 const functions = require("firebase-functions"); // sticking with es5 for now because of time 
+const express = require ("express")
+const cors = require ("cors") // cross orgin resource sharing 
+
+const app = express ()
+app.use (cors())
+
+// set up routes 
+app.get ('/test', (req,res)=> {
+    res.send("Wow, it actually worked!")
+})
+
+app.get ('/another-one', (req,res) => {
+    res.send("This one works, too!")
+})
+
+exports.api= functions.https.onRequest(app) // same thing as ((req, res)=> app (req,res))
+
 
 
               /*whenever there is a request */
-exports.helloThere = functions.https.onRequest  ((req,res) => {
-    res.send("Hello there Firebase Cloud user!")
-})
+// exports.helloThere = functions.https.onRequest  ((req,res) => {
+//     res.send("Hello there Firebase Cloud user!")
+// })
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
